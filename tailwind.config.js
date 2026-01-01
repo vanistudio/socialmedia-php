66137478f2994f193a5f6d69560b2d3e777dd849
Vani <vanixjnk@gmail.com>
2026-01-01 11:05:34 +0700

    feat: con cac 123

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.php",
+    "./views/**/*.php",
+    "./*.php",
+    "./*.html",
+    "./test.html",
+    "./test-tailwind.html",
+    "./public/**/*.php",
+  ],
+}
---
+++ views/contents/_application/AppHome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/public/css/globals.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body class="selection:bg-red-500/20 selection:text-red">
+    <div class="bg-vanixjnk/30 rounded-full text-white p-4">
+        Tailwind is working!
+    </div>
+</body>
+</html>
